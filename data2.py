6c094d69f0a67dbf779692f577b5ae0d6fc72a69
Jackie Wu <wkcn@live.cn>
2016-09-11 11:45:16 +0800

    set flow

---
+++ data2.py
@@ -0,0 +1,42 @@
+#coding=utf-8
+from Node import *
+from Edge import *
+from Flow import *
+
+ox = 30
+oy = 100
+ratio = 1.5
+n1 = Node(ox, oy) 
+n2 = Node(100 * ratio + ox, oy) 
+n3 = Node(200 * ratio + ox, oy) 
+n4 = Node(300 * ratio + ox, oy) 
+
+
+n5 = Node(100 * ratio + ox, 30 * ratio + oy) 
+n6 = Node(200 * ratio + ox, 30 * ratio + oy)
+
+n7 = Node(200 * ratio + ox, oy - 30 * ratio) 
+n8 = Node(200 * ratio + ox, oy + 60 * ratio) 
+
+e1 = GetEdge(n1, n2, length = 500, C = 2000, t = 2)
+e2 = GetEdge(n2, n3, length = 500, C = 1000, t = 2)
+e3 = GetEdge(n3, n4, length = 500, C = 1000, t = 2)
+
+e4 = GetEdgeSingle(n5, n6, length = 600, C = 900, t = 3)
+
+e5 = GetEmptyEdge(n2, n5)
+
+e6 = GetEdge(n3, n6, length = 600, C = 1000, t = 3)
+e7 = GetEdge(n3, n7, length = 600, C = 1000, t = 3)
+e8 = GetEdge(n6, n8, length = 600, C = 2000, t = 3)
+
+Flow.endpoints = [n1, n4, n7, n8]
+
+Q1 = Flow(1000, n1) 
+Q2 = Flow(1000, n4) 
+Q3 = Flow(1000, n7) 
+Q4 = Flow(1000, n8) 
+
+Qs = [Q1, Q2, Q3, Q4]
+Es = [e1, e2, e3, e4, e5, e6, e7, e8]
+Ns = [n1, n2, n3, n4, n5, n6, n7, n8]
--- main.py
+++ main.py
@@ -20,6 +20,29 @@ class MainWindow(QMainWindow):
         self.iterNum = 0
         self.needIterNum = 1000;
 
+        self.labels = [QLabel(self) for _ in range(len(Qs))]
+        self.editBoxs = [QLineEdit(self) for _ in range(len(Qs))]
+        self.button = QPushButton(self)
+        self.buttonEnd = QPushButton(self)
+        ox = 30
+        oy = 400
+        u = 80
+        for i in range(len(Qs)):
+            self.editBoxs[i].move(ox + u * i, oy)
+            self.editBoxs[i].resize(60, 25)
+            self.editBoxs[i].setText("%d" % Qs[i].value)
+            aIntValidator = QIntValidator()
+            aIntValidator.setRange(0, 99999)
+            self.editBoxs[i].setValidator(aIntValidator)
+            self.labels[i].move(ox + u * i + 5, oy - 36)
+            self.labels[i].setText("Q%d" % i)
+        self.button.move(ox, oy + 30)
+        self.button.setText("RestartSim")
+        self.buttonEnd.move(ox + 320, oy + 30)
+        self.buttonEnd.setText("Finish Sim")
+        QObject.connect(self.button, SIGNAL("clicked()"),self.RestartSim) 
+        QObject.connect(self.buttonEnd, SIGNAL("clicked()"),self.FinishSim) 
+
 
     def on_timer(self):
         if self.iterNum < self.needIterNum:
@@ -28,10 +51,19 @@ class MainWindow(QMainWindow):
             self.iterNum += 1
         self.update()
 
-    def RestartSim():
+    def RestartSim(self):
+        for i in range(len(Qs)):
+            Qs[i].value = int(self.editBoxs[i].text()) 
         for es in Es:
             for e in es:
                 e.Q = 0
+        self.iterNum = 0
+
+    def FinishSim(self):
+        while self.iterNum < self.needIterNum:
+            for q in Qs:
+                q.update(self.needIterNum)
+            self.iterNum += 1
 
     def paintEvent(self, e):
         qp = QPainter()
@@ -48,15 +80,39 @@ class MainWindow(QMainWindow):
         for node in Ns:
             qp.drawPoint(node.x, node.y)
 
-        pen = QPen(Qt.red, 6 , Qt.SolidLine)
+        pen = QPen(Qt.black, 6 , Qt.SolidLine)
         qp.setPen(pen)
+        for i in range(len(Qs)):
+            f = Qs[i]
+            p = f.startPoint
+            qp.drawText(p.x - 8, p.y - 8, "Q%d" % i)
+
         for es in Es:
+            u = 0
             for e in es:
                 if e.weight:
                     j1, j2 = e.joints
                     x = (j1.x + j2.x) / 2
                     y = (j1.y + j2.y) / 2
-                    qp.drawText(x, y - 2, str(e.Q))
+                    c = ''
+                    if j1.x < j2.x:
+                        c = '->'
+                    elif j1.x > j2.x:
+                        c = '<-'
+                    elif j1.y < j2.y:
+                        c = 'v'
+                        x += 13
+                    else:
+                        c = '^'
+                        x += 13
+                    if u == 0:
+                        pen = QPen(Qt.red, 6 , Qt.SolidLine)
+                    else:
+                        pen = QPen(Qt.blue, 6 , Qt.SolidLine)
+                    qp.setPen(pen)
+                    text = str(c) + " %d" % e.Q 
+                    qp.drawText(x - 10, y - 2 + u * 18, text)
+                    u += 1
         qp.end()
 
 if __name__ == "__main__":
